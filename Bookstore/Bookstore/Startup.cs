3ce4e8a69d4c73863d487a3ed3ff384e2befec77
Zaibi <zaibi@gmail.com>
2020-07-11 14:00:30 +0500

    Added libraries to use client side library

--- Bookstore/Bookstore/Startup.cs
+++ Bookstore/Bookstore/Startup.cs
@@ -27,6 +27,8 @@ namespace Bookstore
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
